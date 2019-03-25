1728eb6c9fce7728bf4ee3ebca66bec6196e7437
pooja197 <47895436+pooja197@users.noreply.github.com>
2019-03-25 14:04:36 -0400

    set up table

---
+++ Restaurant_Rater/Controllers/RestaurantController.cs
@@ -0,0 +1,20 @@
+﻿using Restaurant_Rater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Restaurant_Rater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantDBContext db = new RestaurantDBContext();
+
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View(db.Restaurants.ToList());
+        }
+    }
+}
--- Restaurant_Rater/Models/Restaurant.cs
+++ Restaurant_Rater/Models/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,16 @@ namespace Restaurant_Rater.Models
 {
     public class Restaurant
     {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+
+
+    }
+
+    public class RestaurantDBContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
